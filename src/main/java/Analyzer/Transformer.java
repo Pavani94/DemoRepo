b3cb09ccd1df57621bbe33c1a34523177c242f64
pavani94 <pvnsgrk@gmail.com>
2021-05-06 11:58:32 +0530

    committed

---
+++ src/main/java/Analyzer/Transformer.java
@@ -0,0 +1,18 @@
+package Analyzer;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.annotations.ITestAnnotation;
+import org.testng.internal.annotations.IAnnotationTransformer;
+
+	public class Transformer implements IAnnotationTransformer {
+
+		
+		public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+			annotation.setRetryAnalyzer(RetryAnalyzer.class);
+		}
+	}
+
+	
+
---
+++ src/test/java/Test/MyTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTest {
+	@Test
+
+	public void Test1() {
+		Assert.assertEquals(false, true);
+		
+	}
+	
+	@Test
+	public void Test2() {
+		Assert.assertEquals(false, true);
+		
+	}
+	
+	@Test
+	public void Test3() {
+		Assert.assertEquals(false, true);
+	}
+}
